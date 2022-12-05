2ab924d5bddc3b9e9c65f6fce96cbdac1e9d7017
MrSoilder23 <72629981+MrSoilder23@users.noreply.github.com>
2022-12-05 21:22:37 +0100

    Changes multiple changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './styles/Footer.css'
+import facebook from '../images/icons8-facebook.svg'
+import instagram from '../images/icons8-instagram.svg'
+import github from '../images/icons8-github.svg'
+
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <div className='footer'>
-        
+      <h1>Sledź nas</h1>
+      <div className="icons">
+        <Link to=''>
+          <img src={facebook} alt="facebook" className='socialIcons'/>
+        </Link>
+        <Link to=''>
+          <img src={instagram} alt="instagram" className='socialIcons'/>
+        </Link>
+        <Link to=''>
+          <img src={github} alt="github" className='socialIcons'/>
+        </Link>
+      </div>
     </div>
   )
 }
