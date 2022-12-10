9d82f5685711608b31761867f3d3ae8b7858ef37
MrSoilder23 <72629981+MrSoilder23@users.noreply.github.com>
2022-12-10 18:20:15 +0100

    Add mobile config

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './styles/Navbar.css'
 import { Link } from 'react-router-dom'
 import Logo from '../images/magnolkaPP2.svg'
+import Hamburger from '../images/hamburgerIcon.svg'
+
 
 function Navbar() {
+
+  const [openLinks, setOpenLinks] = useState(false)
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+    console.log(openLinks)
+  };
+
   return (
     <div className='navbar'>
       <Link to='/' className='navImg'>
         <img src={Logo} alt="Magnolka Falcons" className='logoImg'/>
       </Link>
 
-      <div className="navList">
+      <div className="navList" id={openLinks ? "open" : "close"}>
         <li className='navItem'>
           <Link to='/about' className='navBtn'>
             O nas
@@ -26,12 +36,19 @@ function Navbar() {
             Roboty
           </Link>
         </li>
-        <li className='navItem'>
+        <li className='navItem' >
           <Link to='/contact' className='navBtn'>
             Kontakt
           </Link>
         </li>
       </div>
+
+      <div className='hamburger'>
+        <button className='hamburgerButton' onClick={toggleNavbar}>
+            <img src={Hamburger} alt="" />
+        </button>
+      </div>
+
     </div>
   )
 }
